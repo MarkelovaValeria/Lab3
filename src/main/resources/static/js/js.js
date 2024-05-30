4be3fc8d3ea13db2540ba73b80aa6add378ac43b
MarkelovaValeria <markelovav896@gmail.com>
2024-05-30 15:40:09 +0300

    Lab4

---
+++ src/main/resources/static/js/js.js
@@ -0,0 +1,15 @@
+
+function userPopupMenu(){
+    return{
+        show: false,
+        toggleVisibility() {
+            this.show = !this.show;
+            },
+        close(){
+            this.show = false;
+            },
+        isVisible() {
+            return this.show;
+        }
+    }
+}
